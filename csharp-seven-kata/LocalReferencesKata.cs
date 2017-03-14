31c659207a027460ed82554e6fb9ab60b6c37179
michaelgoin <michaelgoin@gmail.com>
2017-03-14 08:58:28 -0700

    Adds implementation for LocalReferencesKata

--- csharp-seven-kata/LocalReferencesKata.cs
+++ csharp-seven-kata/LocalReferencesKata.cs
@@ -6,9 +6,48 @@ namespace csharp_seven_kata
      * From: https://docs.microsoft.com/en-us/dotnet/articles/csharp/csharp-7#ref-locals-and-returns
      * This feature enables algorithms that use and return references to variables defined elsewhere.
      * These are useful for changing and returning references to specific memory instead of copying memory.
+     * 
+     * Avoiding the overhead of copying resources or pinning memory can be benefitial for high performance applications.
      */
     public class LocalReferencesKata
     {
-        //TODO
+        //KATA: Replace _shootingStar.SetMessage(message) using ref locals and returns with GetMessageByRef()
+        //Note: You'll need to modify ShootingStar.GetMessageByRef() along with LocalReferencesKata.SetMessage(string message)
+        public void SetMessage(string message)
+        {
+            _shootingStar.SetMessage(message);
+        }
+
+        private ShootingStar _shootingStar;
+
+        public LocalReferencesKata()
+        {
+            _shootingStar = new ShootingStar();
+        }
+
+        public ShootingStar GetShootingStar()
+        {
+            return _shootingStar;
+        }
+    }
+
+    public class ShootingStar
+    {
+        private string _message;
+
+        public string GetMessageByRef()
+        {
+            return _message;
+        }
+
+        public void SetMessage(string message)
+        {
+            _message = message;
+        }
+        
+        public override string ToString()
+        {
+            return _message;
+        }
     }
 }
